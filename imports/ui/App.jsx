4c07ac74282c0f0a0c54c0a815473fa7a94545cd
Lisa Gray <gray7@pdx.edu>
2016-07-12 00:12:36 -0700

    fixed some CSS; changed mizzao:bootstrap-3 to twbs:bootstrap

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -11,18 +11,18 @@ export default class App extends Component {
     getReviewContent() {
         return (<ReviewList/>);
     }
-    
+
     render() {
         return (
-			<div className="container">
-				<div id="navBar">  
-					<div id="psuBarIcon"></div>
-					<p id="psuHeaderText">Portland State University Capstone Website</p>
-        </div>
-          <GoogleUIWrapper />
-          <h2>Team Members</h2>
-          <ReviewForm />
-      </div>
+            <div>
+                <div id="banner">
+                    <div id="psuBarIcon"></div>
+                    <p id="psuHeaderText">Portland State University Capstone Website</p>
+                </div>
+                <GoogleUIWrapper />
+                <h2>Team Members</h2>
+                <ReviewForm />
+            </div>
         );
     }
 }
