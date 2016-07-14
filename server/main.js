6e3b842627b5db9831fbf28dfe4df6ab987ed851
Tim Jensen <timothjamesjensen@gmail.com>
2016-07-13 18:58:28 -0700

    changed email check condition

--- server/main.js
+++ server/main.js
@@ -11,8 +11,7 @@ Accounts.onCreateUser((options, user) => {
 Accounts.validateNewUser((user) => {
     let whitelist = Assets.getText('whitelist.csv');
 
-    if(user.services.google.email != undefined
-        && whitelist.search(user.services.google.email) != -1) {
+    if(user.services && user.services.google && user.services.google.email && whitelist.search(user.services.google.email) != -1) {
         return true;
     } else {
         throw new Meteor.Error(403, "This Email Address is not allowed.");
