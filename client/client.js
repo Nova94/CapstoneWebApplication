b50eda1721e2c5eea1851e67ef2dc4d4eac9a66d
Jack Chen <zixuchen@pdx.edu>
2016-07-17 15:53:51 -0700

    Added the router in the server side to display different reviews

--- client/client.js
+++ client/client.js
@@ -4,23 +4,10 @@ Router.route('/reviewForm', function(){
     this.render('ReviewFormTemplate');
 });
 
-Router.route('/reviewForm/:reviewer', function() {
-    this.render('DisplayReviewTemplate', {
-        data: function(){
-            return Reviews.findOne({reviewer: this.params.reviewer});
-        }
-    });
-});
-
 Router.route('/reviewForm/:reviewer/:reviewee', function(){
     this.render('DisplayReviewTemplate', {
         data: function(){
             return Reviews.findOne({reviewer: this.params.reviewer, reviewee: this.params.reviewee});
         }
     });
-})
-Router.route('DisplayAllReviewsTemplate', {
-    data: function(){
-        return {DisplayAllReviewsTemplate: Reviews.find()};
-    }
 })
