192e63ca60b4050f8c17b15346b46ef514bec939
Corey Aing <caing@pdx.edu>
2016-07-16 23:01:05 -0700

    Added Modal and got it to work correctly displaying respective Autoforms

--- imports/ui/student/StudentInfo.jsx
+++ imports/ui/student/StudentInfo.jsx
@@ -1,10 +1,29 @@
 import React, { Component, PropTypes } from 'react';
 import {Button} from 'react-bootstrap';
+import {Modal} from 'react-bootstrap';
 
+import ReviewForm from '../forms/ReviewForm';
 
 export default class StudentInfo extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            showModal: false
+        };
+
+        this.closeModal = this.closeModal.bind(this);
+        this.openModal = this.openModal.bind(this);
+    }
+
+    closeModal()
+    {
+        this.setState({ showModal: false });
+    }
+
+    openModal()
+    {
+        this.setState({ showModal: true });
     }
 
     getMid360Field() {
@@ -32,9 +51,23 @@ export default class StudentInfo extends Component {
             );
         } else {
             return (
-                <Button bsStyle="danger" bsSize="xsmall">
-                    <span className="glyphicon glyphicon-remove"/>
-                </Button>
+                <div>
+                    <Button bsStyle="danger" bsSize="xsmall" onClick={this.openModal}>
+                        <span className="glyphicon glyphicon-remove"/>
+                    </Button>
+                    <Modal show={this.state.showModal} onHide={this.closeModal}>
+                            <Modal.Header>
+                                <Modal.Title>360 Final Review</Modal.Title>
+                            </Modal.Header>
+                            <Modal.Body>
+                                <ReviewForm/>
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button onClick={this.closeModal}>Close</Button>
+                                <Button bsStyle="primary">Submit Form</Button>
+                            </Modal.Footer>
+                    </Modal>
+                </div>
             );
         }
     }
