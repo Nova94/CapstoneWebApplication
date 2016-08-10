8e300d5ccfd8d212e189fa0eafee409c627b6aa9
Stephan Gelever <gelever@pdx.edu>
2016-08-10 11:24:41 -0700

    Add note on future work to do

--- server/methods.js
+++ server/methods.js
@@ -6,6 +6,7 @@ Meteor.users.deny( {
 if(Meteor.isServer) {
 	Meteor.methods({
 		'insertReviewToUser': function(result) {
+			//TODO: check if reviewee is actual user and on same team
 			if (Meteor.userId() && result != undefined) {
 				console.log(result);
 				Meteor.users.update({_id: Meteor.userId()},
